6d7eb44a5b6113058f9fe84d7d7be1d6473d8fa6
Tejas H V <119465817+tj-bli@users.noreply.github.com>
2024-01-09 20:22:40 +0530

    ml lab files

---
+++ EM.py
@@ -0,0 +1,43 @@
+from sklearn.cluster import KMeans
+from sklearn import preprocessing
+from sklearn.mixture import GaussianMixture
+from sklearn.datasets import load_iris
+import sklearn.metrics as sm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset=load_iris()
+# print(dataset)
+dataset
+
+X=pd.DataFrame(dataset.data)
+X.columns=['sepal_lenght','sepal_width','petal_length','petal_width']
+y=pd.DataFrame(dataset.target)
+y.columns=['Targets']
+
+plt.figure(figsize=(14,7))
+colormap=np.array(['red','lime','black'])
+
+plt.subplot(1,3,1)
+plt.scatter(X.petal_length,X.petal_width,c=colormap[y.Targets],s=40)
+plt.title('Real')
+
+
+plt.subplot(1,3,2)
+model=KMeans(n_clusters=3)
+model.fit(X)
+predY=np.choose(model.labels_,[0,1,2]).astype(np.int64)
+plt.scatter(X.petal_length,X.petal_width,c=colormap[predY],s=40)
+plt.title('KMeans')
+
+scaler=preprocessing.StandardScaler()
+scaler.fit(X)
+xsa=scaler.transform(X)
+xs=pd.DataFrame(xsa,columns=X.columns)
+gmm=GaussianMixture(n_components=3)
+gmm.fit(xs)
+y_cluster_gmm=gmm.predict(xs)
+plt.subplot=(1,3,3)
+plt.scatter(X.petal_length,X.petal_width,c=colormap[y_cluster_gmm],s=40)
+plt.title('GMM')
